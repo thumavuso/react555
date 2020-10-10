f14c27b45a31c35f7e3856c348a233463409c677
Thulasizwe <sizwe.mavuso@standardbank.co.za>
2020-10-10 13:29:46 +0200

    My react demo on heroku

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to BuildForSDG.
         </p>
         <a
           className="App-link"
